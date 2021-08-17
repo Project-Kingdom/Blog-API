521816dea9ee76a236e1bafc0183f501160a8e23
Project-Kingdom <kartikgupta673@gmail.com>
2021-08-18 01:43:17 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://dbuser:dbuser@cluster0.9qlzt.mongodb.net/Froshlink?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
+//mongodb://localhost:27017/Leisure
+//mongodb+srv://dbuser:<password>@cluster0.9qlzt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+//mongodb+srv://BlogUser:jAVhPqR9YORwjwXW@cluster0-fc8do.gcp.mongodb.net/BlogDB?retryWrites=true&w=majority
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDb connected");
+});
+
+//middleware
+app.use("/uploads", express.static("uploads"));
+app.use(express.json());
+const userRoute = require("./routes/user");
+app.use("/user", userRoute);
+const profileRoute = require("./routes/profile");
+app.use("/profile", profileRoute);
+const blogRoute = require("./routes/blogpost");
+app.use("/blogPost", blogRoute);
+
+data = {
+  msg: "Welcome on DevStack Blog App development YouTube video series",
+  info: "This is a root endpoint",
+  Working: "Documentations of other endpoints will be release soon :)",
+  request:
+    "Hey if you did'nt subscribed my YouTube channle please subscribe it",
+};
+
+app.route("/").get((req, res) => res.json(data));
+
+app.listen(port, "0.0.0.0", () =>
+  console.log(`welcome your listinnig at port ${port}`)
+);
